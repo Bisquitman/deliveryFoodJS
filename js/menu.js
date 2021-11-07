785e14224b321e8b6e5cead08b1def167bda4f93
Bisquitman <78824565+Bisquitman@users.noreply.github.com>
2021-11-07 18:16:57 +0300

    День 3. Динамическая отрисовка

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,56 @@
-const restaurant = 'food-band';
+const cardsMenu = document.querySelector('.cards-menu');
+const sectionHeading = document.querySelector('.section-heading');
+
+const changeTitle = (restaurant) => {
+  const restaurantTitle = document.querySelector('.restaurant-title');
+  restaurantTitle.textContent = restaurant.name;
+  const cardInfo = document.createElement('div');
+  cardInfo.classList.add('card-info');
+  cardInfo.innerHTML = `
+    <div class="rating">
+      ${restaurant.stars}
+    </div>
+    <div class="price">От ${restaurant.price}&nbsp;₽</div>
+    <div class="category">${restaurant.kitchen}</div>
+  `;
+  sectionHeading.append(cardInfo);
+};
+
 const renderItems = (data) => {
-  data.forEach(element => {
-    console.log(element);
+  data.forEach(({ id, description, image, name, price }) => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = `
+    <img src="${image}" alt="${name}" class="card-image" />
+      <div class="card-text">
+        <div class="card-heading">
+          <h3 class="card-title card-title-reg">${name}</h3>
+        </div>
+        <div class="card-info">
+          <div class="ingredients">${description}
+          </div>
+        </div>
+        <div class="card-buttons">
+          <button class="button button-primary button-add-cart">
+            <span class="button-card-text">В корзину</span>
+            <span class="button-cart-svg"></span>
+          </button>
+          <strong class="card-price-bold">${price}&nbsp;₽</strong>
+        </div>
+      </div>
+    `;
+    cardsMenu.append(card);
   });
 }
 
-fetch(`../db/${restaurant}.json`)
-  .then((response) => response.json())
-  .then((data) => {
-    renderItems(data);
-  });
+if (localStorage.getItem('restaurant')) {
+  const restaurant = JSON.parse(localStorage.getItem('restaurant'));
+  changeTitle(restaurant);
+  fetch(`../db/${restaurant.products}`)
+    .then((response) => response.json())
+    .then((data) => {
+      renderItems(data);
+    });
+} else {
+  window.location.href = 'index.html';
+}
--- js/partners.js
+++ js/partners.js
@@ -1,6 +1,34 @@
+const cardsRestaurants = document.querySelector('.cards-restaurants');
+
 const renderItems = (data) => {
-  data.forEach(element => {
-    console.log(element);
+  data.forEach((item) => {
+    const { image, name, time_of_delivery, stars, price, kitchen, products } = item;
+    const a = document.createElement('a');
+    a.setAttribute('href', 'restaurant.html');
+    a.classList.add('card', 'card-restaurant');
+    a.dataset.products = products;
+    a.innerHTML = `
+      <img src="${image}" alt="${name}" class="card-image" />
+        <div class="card-text">
+          <div class="card-heading">
+            <h3 class="card-title">${name}</h3>
+            <span class="card-tag tag">${time_of_delivery}&nbsp;мин</span>
+          </div>
+          <div class="card-info">
+            <div class="rating">
+              ${stars}
+            </div>
+            <div class="price">От ${price}&nbsp;₽</div>
+            <div class="category">${kitchen}</div>
+          </div>
+        </div>
+    `;
+    a.addEventListener('click', (event) => {
+      event.preventDefault();
+      localStorage.setItem('restaurant', JSON.stringify(item));
+      window.location.href = 'restaurant.html';
+    });
+    cardsRestaurants.append(a);
   });
 }
 
@@ -12,4 +40,4 @@ fetch('https://delivery-food-glo-default-rtdb.firebaseio.com/db/partners.json')
   .catch((error) => {
     console.log('error: ', error);
   })
-  .finally(console.log('finally'));
+  .finally(console.log('final'));
